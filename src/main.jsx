a870b3d36eb8cc11dfab438affcf94f2f485b219
Oğuzhan ÖRS <oguz271402@gmail.com>
2025-05-04 20:59:43 +0300

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,19 @@ import { store } from "./redux/store.js";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
-import App from "./App.jsx";
+import App from "./components/App/App.jsx";
+import { PersistGate } from "redux-persist/integration/react";
+import { Provider } from "react-redux";
+import { store, persistor } from "./redux/store";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </StrictMode>
 );
