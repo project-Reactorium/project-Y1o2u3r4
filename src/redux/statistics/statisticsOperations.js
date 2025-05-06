cec670334347b3c08ba57e64e6144c032c239a3a
Yelenay Uçar <yelenayucr@gmail.com>
2025-05-06 21:11:45 +0300

    Update statisticsOperations.js

--- src/redux/statistics/statisticsOperations.js
+++ src/redux/statistics/statisticsOperations.js
@@ -5,6 +5,7 @@ export const fetchStatistics = createAsyncThunk(
   'statistics/fetchStatistics',
   async ({ month, year }) => {
     const response = await axios.get(`/api/statistics?month=${month}&year=${year}`);
+
     return response.data;
   }
 );
