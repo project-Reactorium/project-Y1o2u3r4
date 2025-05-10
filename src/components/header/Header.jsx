a36dcb76f5f3fe417d64b05850bd25e3a7374ff7
Furkan Sonmez <frkn.snmz46@gmail.com>
2025-05-11 00:09:00 +0300

    header css düzeltildi

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,8 @@
-import { useNavigate } from "react-router-dom";
-import logo from "../../assets/logo.png";
+import HeaderLogoSvg from "./HeaderLogoSvg";
+import HeaderExitSvg from "./HeaderExitSvg";
+import { Link } from "react-router-dom";
 import styles from "./Header.module.css";
-import { useDispatch, useSelector } from 'react-redux';
-import { logOut } from '../../redux/auth/operations';
+import { useSelector } from 'react-redux';
 import { selectUser } from '../../redux/auth/selectors'; 
 import LogOutModal from "../LogOutModal/LogOutModal";
 import { useState } from "react";
@@ -23,21 +23,55 @@ const Header = () => {
   const user = useSelector(selectUser);
   
   return (
-    <header className={styles.Wrapper}>
-      <div className={styles.Left}>
-        <img src={logo} alt="logo" className={styles.Logo} />
-        {/* <span className={styles.BrandName}>Money Guard</span> */}
+    <header className={styles.wrapper}>
+      <div className={styles.left}>
+        <Link to="/" className={styles.logo}>
+          <HeaderLogoSvg />
+          Money Guard
+        </Link>
+        {/* <span className={styles.brandName}>Money Guard</span> */}
       </div>
 
-      <div className={styles.Right}>
-        <span className={styles.User}>{user.username}</span>
-        <button className={styles.ExitButton} onClick={handleLogoutClick}>
-          ⎋ Exit
+      <div className={styles.right}>
+        <span className={styles.user}>{user.username}</span>
+        <button className={styles.exitButton} onClick={handleLogoutClick}>
+          <HeaderExitSvg />
+          <p className={styles.headerExit}>Exit</p>
         </button>
       </div>
       {isModalOpen && <LogOutModal onClose={closeModal} />}
     </header>
   );
+
+   return (
+     <header className={styles.header}>
+       <div className={styles.container}>
+         <Link to="/" className={styles.logo}>
+           <HeaderLogoSvg />
+           Money Guard
+         </Link>
+         <div className={styles.userInfo}>
+           <span className={styles.username}>
+             {username ? username : 'Hello anonymous'}
+           </span>
+           <div>
+             <button
+               className={styles.logoutButton}
+               onClick={handleLogoutClick}
+             >
+               <HeaderExitSvg />
+               <p className={styles.headerExit}>Exit</p>
+             </button>
+           </div>
+         </div>
+         {isModalOpen && <LogOutModal onClose={closeModal} />}
+       </div>
+     </header>
+   );
+
+
+
+
 };
 
 export default Header;
---
+++ src/components/header/HeaderLogoSvg.jsx
@@ -0,0 +1,49 @@
+const HeaderLogo = () => {
+  return (
+    <>
+      {
+        <svg
+          id="logo"
+          width="19"
+          height="23"
+          viewBox="0 0 19 23"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M14.0686 3.08869C12.4052 2.69093 10.7419 1.89542 9.49437 0.702148C8.24686 1.89542 6.58351 2.69093 4.92017 3.08869C5.336 6.6685 6.58351 9.05504 9.49437 11.0438C12.4052 9.05504 14.0686 6.6685 14.0686 3.08869Z"
+            fill="#FFB627"
+          />
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M12.4051 17.899L0.761719 4.77307V11.1372L9.91012 21.0811L12.4051 17.899Z"
+            fill="#FBFBFB"
+          />
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M13.653 16.706L18.6431 11.1374V5.17102L10.7422 13.9217L13.653 16.706Z"
+            fill="#FBFBFB"
+          />
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M14.0688 18.6945V22.2743L18.643 17.1035V13.5237L14.0688 18.6945Z"
+            fill="#FBFBFB"
+          />
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M5.33592 18.6945L0.761719 13.5237V17.1035L5.33592 22.2743V18.6945Z"
+            fill="#FBFBFB"
+          />
+        </svg>
+      }
+    </>
+  );
+};
+
+export default HeaderLogo;
