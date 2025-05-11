4e8b0cc23bf69671c341f7b0d6ed215ec89d51d2
Fatih Demir <fd1134@gmail.com>
2025-05-11 18:21:15 +0300

    Last Modified

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -1,9 +1,9 @@
 import { Formik, Form } from 'formik';
 import { Link } from 'react-router-dom';
+import PasswordStrengthBar from 'react-password-strength-bar-with-style-item';
 
 import { Icon } from '../../Icons';
 import CustomField from '../CustomField/CustomField';
-import Bar from '../Bar/Bar';
 
 import style from './authForm.module.css';
 
@@ -11,14 +11,18 @@ const AuthForm = ({ type, validationSchema, initialValues, onSubmit }) => {
     return (
         <div className={`${type === 'register' ? style.register : style.login} ${style.formWrapper}`}>
             <Formik validationSchema={validationSchema} initialValues={initialValues} onSubmit={onSubmit}>
-                {({ values: { confirmPassword, password } }) => (
+                {({ values: { password } }) => (
                     <Form className={style.form}>
                         <Icon id="#icon-logo_mobile" className={style.icon_mob}></Icon>
                         {type == 'register' && <CustomField type="text" name="username" placeholder="Name" />}
                         <CustomField type="text" name="email" placeholder="E-mail" />
                         <CustomField type="password" name="password" placeholder="Password" />
                         {type === 'register' && <CustomField type="password" name="confirmPassword" placeholder="Confirm password" />}
-                        {type === 'register' && <Bar password={password} confirmPassword={confirmPassword} />}
+                        {type === 'register' && (
+                            <div className={style.passwordStrengthContainer}>
+                                <PasswordStrengthBar password={password} />
+                            </div>
+                        )}
 
                         <button className={`${style.button_main} ${style.colored} `} type="submit">
                             {type === 'register' ? 'Register' : 'Login'}
